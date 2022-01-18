036d1c46ecf14be48b954fac4ab5b3c42714cc82
Vistercg <97806212+Vistercg@users.noreply.github.com>
2022-01-19 00:45:02 +0500

    edits HW

--- src/Hellophp.php
+++ src/Hellophp.php
@@ -7,15 +7,14 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 
-class Hellophp extends Command
+class hellophp extends Command
 {
     protected function configure()
     {
         $this
             ->setName('say_hello')
             ->setDescription('show you say')
-            ->addArgument('text', InputArgument::REQUIRED, 'Pass the text.')
-        ;
+            ->addArgument('text', InputArgument::REQUIRED, 'Pass the text.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -25,5 +24,5 @@ class Hellophp extends Command
 
         return Command::SUCCESS;
     }
-
 }
+
